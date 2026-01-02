1444f880f792765a9642abfe9840725e99f1e6d5
Your Name <you@example.com>
2026-01-02 11:55:50 +0600

    acquaint with Errors and Warnings

---
+++ chapter_1/Error/error.cpp
@@ -0,0 +1,23 @@
+
+
+#include<iostream>
+
+int main(){
+       //!! Errors and Warnings
+     /*
+      Comoile line Errors
+      Runtime Errors
+      warnings
+     */
+
+     //! Comoile line Errors
+     //? ; na thakle 
+    std::cout << "Hello Error code" << std::endl;
+
+    //!!Runtime Errors
+    int value = 7/0;
+    std::cout << "Value : "<< value << std::endl;
+    return 0;
+}
+
+//
--- chapter_1/FirstProgram/hello.cpp
+++ chapter_1/FirstProgram/hello.cpp
@@ -17,6 +17,8 @@ int main()
     std::cout << "Number 1 " << std::endl;
     std::cout << "Number 2" << std::endl;
     std ::cout << "Number 3" << std::endl;
+
+  
     return 0;
     // program end here
 }
