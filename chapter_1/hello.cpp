674394928d4e239366894b9b9bc1a96784586239
Your Name <you@example.com>
2026-01-02 12:03:50 +0600

    Acquainter with c++

---
+++ chapter_1/hello.cpp
@@ -0,0 +1,24 @@
+
+/*
+1.DSA 2.Computers Fundamentals i.Operating Sysrem ii.Computers Network iii.DBMS  iv.-oop  3.Projects  --i.Django+ML ii.C++ Liberary  4.More Topic Graph , Dp , Segment Tree , fenwick Tree
+*/
+// !!One Line comment
+#include <iostream>
+// This brings in the iostrem library
+int main()
+{
+    /*
+
+     !! This is Block comment that englobes multiple lines of text
+
+     std::cout<<"Hello World from free"<<std::endl;
+    */
+
+    std::cout << "Number 1 " << std::endl;
+    std::cout << "Number 2" << std::endl;
+    std ::cout << "Number 3" << std::endl;
+    return 0;
+    // program end here
+}
+
+//
